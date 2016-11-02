f763f9238e3463ae5a3919e1387e8338c24f82e2
Connor Peace <Connorp123@users.noreply.github.com>
2016-11-02 06:35:32 -0700

    Adding heroku support

--- p5/10_Interactive/server.js
+++ p5/10_Interactive/server.js
@@ -1,33 +1,49 @@
 
-// Starts a node express server
-var express = require('express');
+// // Starts a node express server
+// var express = require('express');
+//
+// var app = express();
+// var server = app.listen(3000);
+//
+// app.use(express.static('public'));
+//
+// console.log("Your node server is running");
+//
+// //-------------------------------------------------------------------------------------------------
+//
+// // Creates a socket that's a part of this server
+//
+// var socket = require('socket.io');
+//
+// var io = socket(server);
+//
+// // Sets up a connection event
+// io.sockets.on('connection', newConnection);
+//
+// function newConnection(socket) {
+//     console.log('new connection: ' + socket.id);
+//
+//     socket.on('mouse', mouseMsg);
+//
+//     function mouseMsg(data) {
+//         console.log(data);
+//         socket.broadcast.emit('mouse', data);
+//         // io.sockets.emit('mouse', data);
+//     }
+// }
 
-var app = express();
-var server = app.listen(3000);
 
-app.use(express.static('public'));
 
-console.log("Your node server is running");
 
-//-------------------------------------------------------------------------------------------------
+// HEROKU TUTORIAL
 
-// Creates a socket that's a part of this server
+var http = require('http');
 
-var socket = require('socket.io');
+var server = http.createServer( function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text'});
+    res.end('<h1>Hello World</h1>');
+});
 
-var io = socket(server);
+var port = Number(process.env.PORT || 3000);
 
-// Sets up a connection event
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
-    console.log('new connection: ' + socket.id);
-
-    socket.on('mouse', mouseMsg);
-
-    function mouseMsg(data) {
-        console.log(data);
-        socket.broadcast.emit('mouse', data);
-        // io.sockets.emit('mouse', data);
-    }
-}
+server.listen(port);
