b9cd884e03554cd873bafe41ba93dd9d1616cccf
Connor Peace <Connorp123@users.noreply.github.com>
2017-03-02 18:37:35 -0800

    Added a textbox so the user can edit the text

--- p5/18_TextParticles/sketch.js
+++ p5/18_TextParticles/sketch.js
@@ -3,6 +3,7 @@ let reset    = true;
 let repel    = true;
 let vehicles = [];  // List of Vehicle objects
 let font;
+let textBox1;
 //--------------------------------------------------------------------------------------------------
 
 function preload() {
@@ -14,6 +15,11 @@ function setup() {
     // Creates the canvas
     let myCanvas = createCanvas(1000, 600);
     myCanvas.parent('canvas');
+
+     // Create the buttons
+    textBox1 =  createInput('');
+    textBox1.input(updateText);
+    textBox1.parent('textBox1');
 
     // Gets a list of points from the borders of the text
     let points = font.textToPoints('Connor', 75, height/1.75, 200, {
@@ -46,3 +52,18 @@ function keyPressed() {
     if(key === "R") { repel  = !repel; }
     if(key === "B") { redraw = !redraw; }
 }//-------------------------------------------------------------------------------------------------
+
+function updateText() {
+    // Gets a list of points from the borders of the text
+    let points = font.textToPoints(this.value(), 75, height/1.75, 200, {
+        sampleFactor: 0.25  // sampleFactor ~ frequency of points
+    });
+
+    // Creates a vehicle at each of the points
+    vehicles = [];
+    for(let i = 0; i < points.length; i++) {
+        let pt = points[i];
+        let vehicle = new Vehicle(pt.x, pt.y, 5);
+        vehicles.push(vehicle);
+    }
+}
