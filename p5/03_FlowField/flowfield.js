decb98434ccc4008e3b63cde9ca95db64377bfc8
Connor Peace <connorjpeace@gmail.com>
2019-05-13 23:29:27 -0700

    Updated appearance before showing my class

--- p5/03_FlowField/flowfield.js
+++ p5/03_FlowField/flowfield.js
@@ -61,19 +61,25 @@ function FlowField(r) {
 
   // Renders a vector object 'v' as an arrow and a location 'x,y'
   var drawVector = function(v, x, y, scayl) {
+    
+    // Start the transformation
     push();
     var arrowsize = 4;
+
     // Translate to location to render vector
     translate(x, y);
     stroke(200, 100);
+
     // Call vector heading function to get direction (note that pointing to the right is a heading of 0) and rotate
     rotate(v.heading());
+
     // Calculate length of vector & scale it to be bigger or smaller if necessary
     var len = v.mag() * scayl;
+
     // Draw three lines to make an arrow (draw pointing up since we've rotate to the proper direction)
     line(0, 0, len, 0);
-    line(len,0,len-arrowsize,+arrowsize/2);
-    line(len,0,len-arrowsize,-arrowsize/2);
+    // line(len,0,len-arrowsize,+arrowsize/2);
+    // line(len,0,len-arrowsize,-arrowsize/2);
     pop();
   };
 }
--- p5/03_FlowField/sketch.js
+++ p5/03_FlowField/sketch.js
@@ -1,38 +1,53 @@
-var debug = false;   // Draws debug stuff
+var debug = true;   // Draw the flow field 
+var redrawBackground = true; // Re-draw the background every frame
 var flowField;      // flowField object
 var vehicles = [];  // List of Vehicle objects
+var NUM_VEHICLES = 100;
 
 function setup() {
   // Creates the canvas
-  var myCanvas = createCanvas(600, 300);
+  var myCanvas = createCanvas(960, 520);
+  // var myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.parent('canvas');
 
   // Makes a new flowField
-  flowField = new FlowField(20);
+  flowField = new FlowField(30);
 
   // Makes a bunch of vehicles with random values
-  for (var i = 0; i < 120; i++) {
+  for (var i = 0; i < NUM_VEHICLES; i++) {
     vehicles.push(new Vehicle(random(width), random(height), random(2, 5), random(0.1, 0.5)));
   }
 }
 
 function draw() {
-  background(51);
+
+  // Re-draw the background
+  if (redrawBackground) background(51);
+
   // Display the flowField in "debug" mode
   if (debug) flowField.display();
+
   // Tell all the vehicles to follow the flow field
   for (var i = 0; i < vehicles.length; i++) {
     vehicles[i].follow(flowField);
     vehicles[i].run();
   }
-
 }
 
 // Key-binds
 function keyPressed() {
   // SPACE - Debug
-  if (key == ' ') {
+  if (key === ' ') {
     debug = !debug;
+  } else if (key === 'p' || key === 'P') {
+    alert(
+      "Controls:\n"
+      + "Click to create a new flow field\n"
+      + "Hit space to show the flow field\n"
+      + "Hit 'b' to stop the background redraw"
+    );
+  } else if (key === 'b' || key === 'B') {
+    redrawBackground = !redrawBackground;
   }
 }
 
