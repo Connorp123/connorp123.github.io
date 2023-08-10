05a35d978b250662a20645d875828cf8a992b85a
Connor <connorjpeace@gmail.com>
2023-08-09 21:59:36 -0700

    fix action

--- noc/00-dont-use.js
+++ noc/00-dont-use.js
@@ -1,3 +1,7 @@
+const actions = {
+    "00-dont-use.js": () => alert("bad!")
+};
+
 const getJsonFiles = async () => {
     const response = await fetch("/noc/jsfiles.json");
     const jsFiles  = await response.json();
@@ -9,6 +13,16 @@ const getJsonFiles = async () => {
         option.value       = file;
         option.textContent = file;
         selector.appendChild(option);
+    });
+
+    selector.addEventListener("change", function () {
+        const selectedFile = this.value; // Get the selected filename
+        const action       = actions[selectedFile];
+        if (action) {
+            action(); // Calls the function mapped to the selected file
+        } else {
+            console.log("No action defined for this file");
+        }
     });
 };
 
