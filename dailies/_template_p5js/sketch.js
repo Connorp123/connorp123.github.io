a89c262b6ad801f81e33fbcc64e560d947deb0e8
Connor Peace <connorjpeace@gmail.com>
2024-01-07 13:59:26 -0800

    updated template and create-index.bat

--- dailies/_template_p5js/sketch.js
+++ dailies/_template_p5js/sketch.js
@@ -1,13 +1,14 @@
 import { createGui } from "../../helpers/gui-helpers.js";
+import { getGifName } from "../../helpers/modules/display-helpers-modules.js";
 
 const createClass = ({p}) => {
     return class MyClass {
-        constructor() {
-            this.pos = [p.mouseX, p.mouseY];
+        constructor({x, y}) {
+            this.pos = [x, y];
         }
 
-        update() {
-            this.pos = [p.mouseX, p.mouseY];
+        update({x, y}) {
+            this.pos = [x, y];
         }
 
         draw() {
@@ -30,17 +31,28 @@ export const sketch = (p) => {
     const MyClass = createClass({p});
 
     // Global vars
-    let classInstance;
+    const NUM_INSTANCES = 1;
+    let classInstances  = [];
+
 
     p.setup = () => {
         canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         p.frameRate(144);
-        classInstance = new MyClass();
+        for (let i = 0; i < NUM_INSTANCES; i++) {
+            classInstances.push(new MyClass({x: p.mouseX, y: p.mouseY}));
+        }
     };
 
     p.draw = () => {
         if (state.redrawBackground) p.background(0);
-        classInstance.update();
-        classInstance.draw();
+        classInstances.forEach(classInstance => {
+            classInstance.update({x: p.mouseX, y: p.mouseY});
+            classInstance.draw();
+        });
+    };
+
+    p.keyPressed = () => {
+        const key = p.key.toLowerCase();
+        if (key === "s") p.save(`${getGifName()}.png`);
     };
 };
